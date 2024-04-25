94b70374f8f1fc4d59bc3b1e9b77c487c0aeff5c
dypaq <168001030+dypaq@users.noreply.github.com>
2024-04-26 04:45:27 +0530

    Add files via upload

---
+++ name.R
@@ -0,0 +1,37 @@
+cfa (1).R
+
+cluster (1).R
+
+clusteranalysis_sale.R
+
+clusteranalysis_us.R
+
+conjoint.R
+
+faonfa.R
+
+faontrain.R
+
+fpca (1).R
+
+kernal pca (1).R
+
+ldaqda.R
+
+logisticairline.R
+
+logisticfa.R
+
+logisticmobile.R
+
+logisticmulti.R
+
+logistictrain.R
+
+market.R
+
+pcaapple.R
+
+pcafa.R
+
+sem.R
